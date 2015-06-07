9bfdc2488eff285a595bd085fdb3eb5e4013b289
jackhorton <jackpeterhorton@gmail.com>
2015-06-07 02:49:34 -0700

    Initial prototype using soundcloud JS SDK

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
 SC.initialize({
     client_id: "578875cadc55ccccac217296e207d111",
+    client_secret: "1e27455c2b3e6ccdd2e14c70ab33ed5c",
     redirect_uri: "http://localhost:8000/connect"
 });
 
 SC.connect(function() {
-    SC.get('/me', function(me) {
-        alert('Hello, ' + me.username);
+    SC.get('/me/favorites', function(favorites) {
+        console.log(favorites);
+        SC.stream(favorites[0].stream_url, function(sound) {
+            sound.play();
+        });
     });
 });
