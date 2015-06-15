abcaa7a92ec5ad9a51dd5838c1e7e2c2eb796d4a
jackhorton <jackpeterhorton@gmail.com>
2015-06-14 23:38:06 -0700

    Using Montserrat font

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 // TODO: take this out for production
 app.use(express.static("src"));
+app.use("/css/lib", express.static("src/lib"));
 app.use("/css", express.static("public/css"));
 
 app.get("/connect", function(req, res) {
