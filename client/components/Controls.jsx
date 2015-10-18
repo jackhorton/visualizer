84b4f2c49eb70a731141ec5999c3ba3f14bb922d
jackhorton <jackpeterhorton@gmail.com>
2015-10-17 20:47:48 -0400

    Added switch to toggle between visualizer and tracklist views

--- client/components/Controls.jsx
+++ client/components/Controls.jsx
@@ -14,11 +14,22 @@ const Controls = React.createClass({
         actions.pause();
     },
 
+    toggleView() {
+        const main = document.getElementsByTagName('main')[0];
+        const appState = main.getAttribute('data-app-state');
+        if (appState === 'visualizer') {
+            main.setAttribute('data-app-state', 'tracklist');
+        } else {
+            main.setAttribute('data-app-state', 'visualizer');
+        }
+    },
+
     render() {
         return (
             <div className="controls">
                 <span onClick={this.play}>Play</span>
                 <span onClick={this.pause}>Pause</span>
+                <span onClick={this.toggleView}>Toggle View</span>
             </div>
         );
     }
